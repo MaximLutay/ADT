09f4cf3f3d18a652c554680b3228e2789b7cd503
Maxim Lutay <maksym.lutai@nure.ua>
2016-06-03 13:25:46 +0300

    Update deqHeader.hpp

--- lb_1/deqHeader.hpp
+++ lb_1/deqHeader.hpp
@@ -1,1 +1,30 @@
+#pragma once
+#ifndef __Header__
+#define  __Header__
 
+
+struct IntegerDeque;
+IntegerDeque * createIntDeq(int _m_block_size);
+void directoryGrowTop(IntegerDeque * _deq);
+void directoryGrowBottom(IntegerDeque * _deq);
+void directoryDelete(IntegerDeque * _deq);
+void push_front(IntegerDeque *_deq, int _value);
+void push_back(IntegerDeque *_deq, int _value);
+void directoryDecreaseTop(IntegerDeque *_deq);
+void directoryDecreaseBottom(IntegerDeque *_deq);
+int pop_front(IntegerDeque *_deq);
+int pop_back(IntegerDeque *_deq);
+int back(IntegerDeque *_deq);
+int front(IntegerDeque *_deq);
+bool is_empty(IntegerDeque *_deq);
+
+
+
+
+
+
+
+
+
+
+#endif
