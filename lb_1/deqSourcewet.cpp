35d17822ab2bd671e5fbafe913762238f643a9de
Maxim Lutay <maksym.lutai@nure.ua>
2016-06-03 13:17:33 +0300

    updated
    v2.0.0

--- lb_1/deqSourcewet.cpp
+++ lb_1/deqSourcewet.cpp
@@ -115,20 +115,55 @@ void directoryDecreaseBottom(IntegerDeque *_deq){
 
 
 
-
-
-
 int pop_front(IntegerDeque *_deq) {
 	if (_deq->m_frontUsed == 1) {
-		directoryDecreaseTop;
+		int temoint = _deq->m_pDirectory[0][0];
+		directoryDecreaseTop(_deq);
 		_deq->m_frontUsed = _deq->m_blockSize;
+		return temoint;
 	}else {
 		int *temp = new int[_deq->m_blockSize];
-		
+		memcpy((temp + _deq->m_blockSize - _deq->m_frontUsed), (*(_deq->m_pDirectory + 0) + _deq->m_blockSize - _deq->m_frontUsed), sizeof(int)*(_deq->m_frontUsed - 1));
+		_deq->m_frontUsed--;
+
+		int temoint = front(_deq);
+
+		_deq->m_pDirectory[0] = temp;
+		return temoint;
 	};
 };
 
-int pop_back() {
+int pop_back(IntegerDeque *_deq) {
+	if (_deq->m_backUsed == 1) {
+		int temoint = _deq->m_pDirectory[_deq->m_backBlockIndex - 1][0];
+		directoryDecreaseBottom(_deq);
+		_deq->m_backUsed = _deq->m_blockSize;
+		return temoint;
+	}else {
+		int temoint = back(_deq);
+		int *temp = new int[_deq->m_blockSize];
+		memcpy(  temp, *(_deq->m_pDirectory + _deq->m_backBlockIndex) , sizeof(int)*(_deq->m_backUsed - 2));
+		_deq->m_backUsed--;
+		return temoint;
+	};
+};
+
+
+int back(IntegerDeque *_deq) {
+	return _deq->m_pDirectory[_deq->m_backBlockIndex - 1][_deq->m_backUsed - 1];
+};
+
+
+int front(IntegerDeque *_deq) {
+	return _deq->m_pDirectory[0][_deq->m_blockSize - _deq->m_frontUsed - 1];
+};
+
+bool is_empty(IntegerDeque *_deq) {
+	if (( _deq->m_directorySize == 1) && (_deq->m_frontUsed == 0)) {
+		return true;
+	};
+	return false;
+
 };
 
 
